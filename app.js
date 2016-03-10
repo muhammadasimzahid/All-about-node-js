9aec6873c860863d3a318ce6409e090178927c4c
Muhammad Asim Zahid <muhammadasimzahid@gmail.com>
2016-03-10 08:43:31 +0500

    work with defaulf templating engine jade 3 example 1 : simple loading from views, 2 : send a value form server to a views and 3 : create a layout file from which other jade file can extend themselve by using block keywork

--- app.js
+++ app.js
@@ -5,35 +5,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 
 app.get('/', function(req, res){
-    res.send('Hello, Express!');
-});
-
-app.get('/hi', function(req, res){
-    var message = [
-        "<h1>Hello, Express!</h1>",
-        "<p>Welcome to Building Web Apps in Node.js with Express.</p>",
-        "<p>You will love Express becaues it's : </p>",
-        "<ul>",
-        "<li>Fast</li>",
-        "<li>Fun</li>",
-        "<li>Flexible</li>",
-        "</ul>"
-    ].join('\n');
-    res.send(message);
-});
-
-app.get('/users/:id', function(req, res){
-    res.send('<h1>Hello, User # ' + req.params.id + '!</h1>');
-});
-
-app.post('/users', function(req, res){
-    console.log(req.body);
-    res.send('Creating a new user with username : ' + req.body.username);
-});
-
-app.get(/\/bakri\/(\d*)\/(edit)\/(asim)/, function(req, res){
-    console.log(req.params);
-    res.send('Hello, bakri');
+    res.render('home.jade', { title: "Passing data from res.render() function to home.jade file." });
 });
 
 app.listen(app.get('port'), function(){
