4aaba3556a6a8bd8d9f8550b4bcc206413286830
Muhammad Asim Zahid <muhammadasimzahid@gmail.com>
2016-03-11 23:14:58 +0500

    work with both cookie and session at the same time

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var cookieParser = require('cookie-parser');
 
 var app = express();
 
@@ -10,6 +11,7 @@ app.set('view engine', 'jade');
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
+app.use(cookieParser());
 app.use(session({ 
     secret: 'this is secret used for security purpose',
     resave: true,
@@ -17,17 +19,19 @@ app.use(session({
 }));
 
 app.get('/name', function(req, res){
-    res.send(req.session.name);// through session
+    res.send('Name through <b>Cookie : </b> ' + req.cookies.name + ' and Name through <b>Session : </b>' + req.session.name + ' !')
 });
 
 app.get('/name/:name', function(req, res){
+    res.cookie('name', req.params.name);//set through cookie
     req.session.name = req.params.name; //set through session
     res.send('Set');
 });
 
 app.get('/clear', function(req, res){
+    res.clearCookie('name');// clear a cookie
     req.session.destroy();
-    res.send('Destroyed');
+    res.send('Cleard Name form session and cookies');
 })
 
 
