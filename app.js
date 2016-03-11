33f5a65115b2a327fd24960753c5c6553e14a983
Muhammad Asim Zahid <muhammadasimzahid@gmail.com>
2016-03-11 14:57:29 +0500

    learn how to work with diffrent response methods that expresss offers mostlyare shown in app.js

--- app.js
+++ app.js
@@ -10,27 +10,21 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-    //res.send(req.get('user-agent')); //show the user agent header
-    //res.send(req.header('user-agent')); // show the user agent header
-    //res.send(req.accepted); this is not working in express 4 now use the following: but is used to show which types are accepted
-    //res.send(req.accepts(['html', 'text', 'json'])); // check which one is accepted
-    //res.send(req.acceptedCharsets); //this is also not working // used to check which charset are supported
-    //res.send(req.acceptsCharsets('utf-8') ? 'yes' : 'no'); // work this way by check by ternary operator yes or no
-    //res.send(req.acceptedLanguages); also not working use following // show which languages are accepted.
-    //res.send(req.acceptsLanguages('en') ? 'yes' : 'no'); //is english is accepted
-    //res.send(req.acceptsLanguages('urdu') ? 'yes' : 'no'); // is urdu accepted
-    //res.send(req.acceptsLanguages('en-US') ? 'yes' : 'no'); // is eng of US accept
-    //res.send(req.acceptsLanguages('fr') ? 'yes' : 'no'); // is french is accepted
+    //res.send(404, 'Hello, Express and I am new to express.');
+    //res.render('layout');
+    //res.json({name: "Muhammad Asim Zahid"});
+    //res.type('image/png').send('this is a picture.');
+    //res.format({
+    //    html: function () { res.send('<h1>Hello, Express!</h1>'); },
+    //    json: function () { res.json({message: 'Hello, Express!'}); },
+    //    text: function () { res.send('Hello, Express!'); }
+    //});
+    res.send('Hello, Express!');
 });
 
-app.get('/name/:name', function(req, res){
-    res.send(req.params.name);
-});
-
-app.get('/nam/:nam?', function(req, res){
-    res.send(req.param('nam', 'Default Value'));
-});
-
+//app.get('/user', function(req, res){
+//    res.redirect('/');
+//});
 
 app.listen(app.get('port'), function(){
     console.log('Express server listening on Port : ' + app.get('port'));
