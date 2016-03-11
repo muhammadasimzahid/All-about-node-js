b2c7550f75c99a72893947a16d79223fb559d7ea
Muhammad Asim Zahid <muhammadasimzahid@gmail.com>
2016-03-11 22:26:10 +0500

    learn how to work with cookies in node using express js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -7,20 +9,21 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.use(bodyParser.json());
-//app.use(express.static(__dirname + '/public'));
-
-var count = 0;
-
-app.get('/hello.txt', function(req, res, next){
-    count++;
-    next();
-});
-
 app.use(express.static(__dirname + '/public'));
+app.use(cookieParser());
 
-app.get('/total', function(req, res){
-    res.send('Total times its watched is : : ' + count);
+app.get('/name', function(req, res){
+    res.send('Your saved name is : <h2>' + req.cookies.name + '!</h2>');
 });
+
+app.get('/name/:name', function(req, res){
+    res.cookie('name', req.params.name/*, {maxAge: 9000}*/).send('To check the set cookie <a href="/name">Get set Name .</a>');
+});
+
+app.get('/clear', function(req, res){
+    res.clearCookie('name').send(req.cookies.name);
+})
+
 
 app.listen(app.get('port'), function(){
     console.log('Express server listening on Port : ' + app.get('port'));
