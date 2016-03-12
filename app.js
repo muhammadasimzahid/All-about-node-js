3ef83e0ec3f599297ac7cf08842eef2d48d5c54a
Muhammad Asim Zahid <muhammadasimzahid@gmail.com>
2016-03-12 10:22:42 +0500

    learn how to wrok with error handling in express (showing simple errror if a route not found using middleware, if certain condition matched and want to change that request to our middleware using the next() option, and last learn how to add your own specific error handle middleware and how to work with them )

--- app.js
+++ app.js
@@ -11,29 +11,40 @@ app.set('view engine', 'jade');
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
-app.use(cookieParser());
-app.use(session({ 
-    secret: 'this is secret used for security purpose',
-    resave: true,
-    saveUninitialized: true
-}));
 
-app.get('/name', function(req, res){
-    res.send('Name through <b>Cookie : </b> ' + req.cookies.name + ' and Name through <b>Session : </b>' + req.session.name + ' !')
+app.get('/', function(req, res){
+    res.send('index');
 });
 
-app.get('/name/:name', function(req, res){
-    res.cookie('name', req.params.name);//set through cookie
-    req.session.name = req.params.name; //set through session
-    res.send('Set');
+app.get('/users/:username', function(req, res, next){
+    if(req.params.username === 'asim'){
+        next();
+    }else{
+        res.send(req.params.username + '\'s profile.')
+    }
 });
 
-app.get('/clear', function(req, res){
-    res.clearCookie('name');// clear a cookie
-    req.session.destroy();
-    res.send('Cleard Name form session and cookies');
-})
+//same as above just used to show the special error middleware.
+app.get('/bakri/:name', function(req, res, next){
+    if(req.params.name === 'leli'){
+        var error = new Error('User with ' + req.params.name + ' username not found in database.');
+        error.status = 404;
+        next(error);
+    }else{
+        res.send(req.params.name + '\'s profile.')
+    }
+});
 
+
+app.use(function(error, req, res, next){
+    res.status(error.status);
+    res.send(error.message);
+});
+
+
+app.use(function(req, res){
+    res.send(404, '4-o-4 error');
+});
 
 app.listen(app.get('port'), function(){
     console.log('Express server listening on Port : ' + app.get('port'));
