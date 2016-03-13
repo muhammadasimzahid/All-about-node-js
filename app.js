135b412637f3a1ee8a386c3b80b39435e93be9b1
Muhammad Asim Zahid <muhammadasimzahid@gmail.com>
2016-03-13 14:07:31 +0500

    learn how to use mongoose and also create a full fledge user management website and that's it

--- app.js
+++ app.js
@@ -1,50 +1,108 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var session = require('express-session');
-var cookieParser = require('cookie-parser');
-
+var mongoose = require('mongoose');
+var methodOverride = require('method-override');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
+app.use(methodOverride('_method'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-    res.send('index');
+
+
+
+mongoose.connect('mongodb://localhost/helloExpress');
+
+var UserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number
 });
 
-app.get('/users/:username', function(req, res, next){
-    if(req.params.username === 'asim'){
-        next();
-    }else{
-        res.send(req.params.username + '\'s profile.')
-    }
+var Users = mongoose.model('Users', UserSchema);
+
+
+//show all users
+app.get('/users', function(req, res){
+    Users.find({}, function(err, docs){
+        if(err){
+            res.json(err);
+        }else{
+            res.render('users/index', {users: docs});
+        }
+    });
+});
+//new user
+app.get('/users/new', function(req, res){
+    res.render('users/new');
+});
+//post new user
+app.post('/users', function(req, res){
+    var b = req.body;
+    new Users({
+        name: b.name,
+        email: b.email,
+        age: b.age
+    }).save(function(err, docs){
+        if(err){
+            res.json(err);
+        }else{
+            res.redirect('/users/' + b.name);
+        }
+    });
+});
+//create a param middleware
+app.param('name', function(req, res, next, name){
+    Users.find({name: name}, function(err, docs){
+        if(err){
+            res.send('User with ' + name + ' username not found.!');
+        }else{
+            req.user = docs[0];
+            next();
+        }
+    });
+});
+//show single user
+app.get('/users/:name', function(req, res){
+    res.render('users/show', {user: req.user});
+});
+//edit
+app.get('/users/:name/edit', function(req, res){
+    res.render('users/edit', {user: req.user});
+});
+//update
+app.put('/users/:name', function(req, res){
+    var b = req.body;
+    Users.update(
+        {name: req.params.name},
+        {name: b.name, email: b.email, age: b.age},
+        function(err){
+            res.redirect('/users/' + b.name);
+        }
+    );
+});
+//delete
+app.delete('/users/:name', function(req, res){
+    Users.remove({name: req.params.name}, function(err){
+        res.redirect('/users');
+    });
 });
 
-//same as above just used to show the special error middleware.
-app.get('/bakri/:name', function(req, res, next){
-    if(req.params.name === 'leli'){
-        var error = new Error('User with ' + req.params.name + ' username not found in database.');
-        error.status = 404;
-        next(error);
-    }else{
-        res.send(req.params.name + '\'s profile.')
-    }
-});
 
 
-app.use(function(error, req, res, next){
-    res.status(error.status);
-    res.send(error.message);
-});
 
 
-app.use(function(req, res){
-    res.send(404, '4-o-4 error');
-});
+
+
+
+
+
+
 
 app.listen(app.get('port'), function(){
     console.log('Express server listening on Port : ' + app.get('port'));
